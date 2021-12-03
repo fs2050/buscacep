5aebc7f9baf437e117b0ff0d092123f99ba5a7f6
fs2050 <fabiolfs76@hotmail.com>
2021-12-03 04:32:51 -0300

    Inserindo campo complemento no aplicativo e no banco de dados

--- resources/views/buscar.blade.php
+++ resources/views/buscar.blade.php
@@ -59,7 +59,7 @@
                 //Valida o formato do CEP.
                 if(validacep.test(cep)) {
 
-                    
+
                     //Cria um elemento javascript.
                     var script = document.createElement('script');
 
@@ -91,16 +91,7 @@
             <label>Cep:
             <input name="cep" type="text" id="cep" value="" size="10" maxlength="9"
                    onblur="pesquisacep(this.value);" /></label><br />
-            <label>Rua:
-            <input name="rua" type="text" id="rua" size="60" /></label><br />
-            <label>Bairro:
-            <input name="bairro" type="text" id="bairro" size="40" /></label><br />
-            <label>Cidade:
-            <input name="cidade" type="text" id="cidade" size="40" /></label><br />
-            <label>Estado:
-            <input name="uf" type="text" id="uf" size="2" /></label><br />
-            <label>IBGE:
-            <input name="ibge" type="text" id="ibge" size="8" /></label><br />
+           
 
             <button type="submit" class="btn btn-success">enviar</button></form>
 

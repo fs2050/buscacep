d2ce897e37e766a88bc704207727e7bdafbd7a98
fs2050 <fabiolfs76@hotmail.com>
2021-12-03 00:59:38 -0300

    deploy

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
         <form action="{{route('search')}}">
             <div class="form-group">
                 <label>CEP</label>
-                <input type="text" class="form-control" name="cep" required/>
+        {{--         <input type="text" class="form-control" name="cep" required/> --}}
                 <input name="cep" type="text" id="cep" value="" size="10" maxlength="9"
                 onblur="pesquisacep(this.value);" /></label><br />
             </div>

166bce2d30f02f651c0607b60e4a94f41ac66b0e
fs2050 <fabiolfs76@hotmail.com>
2021-12-03 04:42:49 -0300

    Inserindo campo complemento no aplicativo e no banco de dados

--- resources/views/buscar.blade.php
+++ resources/views/buscar.blade.php
@@ -9,20 +9,20 @@
 
         function limpa_formulário_cep() {
                 //Limpa valores do formulário de cep.
-                document.getElementById('rua').value=("");
+             /*    document.getElementById('rua').value=("");
                 document.getElementById('bairro').value=("");
                 document.getElementById('cidade').value=("");
-                document.getElementById('uf').value=("");
+                document.getElementById('uf').value=(""); */
         /*         document.getElementById('ibge').value=(""); */
         }
 
         function meu_callback(conteudo) {
             if (!("erro" in conteudo)) {
                 //Atualiza os campos com os valores.
-                document.getElementById('rua').value=(conteudo.logradouro);
+            /*     document.getElementById('rua').value=(conteudo.logradouro);
                 document.getElementById('bairro').value=(conteudo.bairro);
                 document.getElementById('cidade').value=(conteudo.localidade);
-                document.getElementById('uf').value=(conteudo.uf);
+                document.getElementById('uf').value=(conteudo.uf); */
              /*    document.getElementById('ibge').value=(conteudo.ibge); */
             } //end if.
             else {
@@ -81,7 +81,7 @@
             <label>Cep:
             <input name="cep" type="text" id="cep" value="" size="10" maxlength="9"
                    onblur="pesquisacep(this.value);" /></label><br />
-            <label>Rua:
+           {{--  <label>Rua:
             <input name="rua" type="text" id="rua" size="60" /></label><br />
             <label>Bairro:
             <input name="bairro" type="text" id="bairro" size="40" /></label><br />
@@ -89,7 +89,7 @@
             <input name="cidade" type="text" id="cidade" size="40" /></label><br />
             <label>Estado:
             <input name="uf" type="text" id="uf" size="2" /></label><br />
-            <label>IBGE:
+            <label>IBGE: --}}
           {{--   <input name="ibge" type="text" id="ibge" size="8" /></label><br /> --}}
 
             <button type="submit" class="btn btn-success">enviar</button></form>

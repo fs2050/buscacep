74997eab7c17c0246dbb65cf0a641fbb5a0fdec7
fs2050 <fabiolfs76@hotmail.com>
2021-12-03 04:23:03 -0300

    Inserindo campo complemento no aplicativo e no banco de dados

--- resources/views/buscar.blade.php
+++ resources/views/buscar.blade.php
@@ -78,21 +78,22 @@
         </script>
         </head>
         <body>
-        <div class="container">
+            <div class="container">
 
-            <h1 class="text-center">BUSCAR CEP</h1>
-            <form method="get" action="{{route('salvar')}}">
-                <div class="form-group">
-                    <label>CEP</label>
-                    {{-- <input type="text" class="form-control" name="cep" required/> --}}
-                    <input name="cep" type="text" id="cep" value="" size="10" maxlength="9"
-                    onblur="pesquisacep(this.value);" /></label><br />
+                <h1 class="text-center">BUSCAR CEP</h1>
+                <form method="get" action="{{route('search')}}">
+                    <div class="form-group">
+                        <label>CEP</label>
+                        {{-- <input type="text" class="form-control" name="cep" required/> --}}
+                        <input name="cep" type="text" id="cep" value="" size="10" maxlength="9"
+                        onblur="pesquisacep(this.value);" /></label><br />
 
-                </div>
-                <button type="submit" class="btn btn-success">enviar</button>
-            </form>
-        </div>
-</body>
+                    </div>
+                    <button type="submit" class="btn btn-success">enviar</button>
+                </form>
+            </div>
+    </body>
+
 
 
 
